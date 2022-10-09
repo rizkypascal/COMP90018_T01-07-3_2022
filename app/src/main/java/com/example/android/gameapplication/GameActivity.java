fce1b1a7b3c1c4cb7a79f1b94f4c59bf12b31033
YuhanGu <yuhan.gu.z@outlook.com>
2022-10-10 03:48:12 +1100

    updated Context with jumper and boards(static)

--- app/src/main/java/com/example/android/gameapplication/GameActivity.java
+++ app/src/main/java/com/example/android/gameapplication/GameActivity.java
@@ -32,21 +32,16 @@ import com.example.android.gameapplication.Sensors.OrientationSensor;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class GameActivity extends AppCompatActivity implements Runnable {
+public class GameActivity extends AppCompatActivity {
 
+    private GameContext gameContext;
     private OrientationSensor orientationSensor;
-    private Jumper jumper;
-    private ArrayList<Board> bars;
-    private Boolean isPlaying = true;
+
     private int screenX, screenY = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /** Init sensor variables*/
-        orientationSensor = new OrientationSensor(this);
-        EventBus.getDefault().register(this);
 
         setContentView(R.layout.activity_game);
         // get game activity
@@ -57,14 +52,10 @@ public class GameActivity extends AppCompatActivity implements Runnable {
         screenX = metrics.widthPixels;
         screenY = metrics.heightPixels;
 
-        // initialize jumper
-        jumper = new Jumper(this,100,500,100,10f,screenX,R.drawable.jumperone);
-        constraintLayout.addView(jumper);
+        gameContext = new GameContext (this, screenX, screenY);
+        constraintLayout.addView(gameContext);
 
-        bars = random_generate(screenY, screenX);
-        for (Board bar : bars){
-            constraintLayout.addView(bar);
-        }
+
     }
 
     @Override
@@ -73,88 +64,6 @@ public class GameActivity extends AppCompatActivity implements Runnable {
         orientationSensor.disableSensor();
         super.onDestroy();
     }
-
-    @Override
-    public void run() {
-        while (isPlaying) {
-            checkJumper();
-        }
-    }
-
-
-    /**
-     * @author Changwen Li
-     * @description Please get the value of changed sensor signal here. You may change the name of function.
-     * @param OrientationEvent see OrientationMessage.java
-     * */
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    public void orientationUpdate(OrientationMessage OrientationEvent) { // place to get sensor value from orientation
-        Log.d("[Subscription]" , "Orientations: " + String.valueOf(OrientationEvent.getOrientations()[2]));
-        Float moveX = 50*OrientationEvent.getOrientations()[2];
-        jumper.move(moveX,10f);
-
-        // not a good solution, as collision detection should be done within the context class
-        for (Board bar : bars){
-            if(CollisionUtils.JumperBoardCollision(jumper,bar))
-            {
-                jumper.setSpeedY(20f);
-            }
-            bar.move(10f,0f);
-        }
-
-
-    }
-
-    private ArrayList<Board> random_generate(int startY, int width){
-        ArrayList<Board> newbars = new ArrayList<>();
-        Random random = new Random();
-        int y = startY;
-        int x = width/5;
-        while (y > 0){
-
-            Board bar = new StaticBoard(this,x * random.nextInt(5),y,250,width,R.drawable.basic_board);
-            newbars.add(bar);
-//            Log.i(String.valueOf(bar.y),String.valueOf(bar.height));
-            y -= bar.getBoardHeight()*5*random.nextInt(2);
-        }
-        return newbars;
-    }
-
-    private Boolean checkJumper(){
-        Log.i("reminder","reach "+jumper.getPosY()+" screen" + screenY);
-        if (jumper.getPosY()*2 < screenY){
-            Log.i("i", "1/2");
-            update();
-        }
-        if (jumper.getPosY() > screenY){
-            return false;
-        }
-        return true;
-    }
-
-    private void update () {
-        this.isPlaying = false;
-        // update the bars downward half screen
-        for (Board bar : bars){
-            bar.setPosY(bar.getPosY()+screenY/2);
-        }
-        Log.i("i","update bar");
-
-        // remove the bar with y out of screen
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            bars.removeIf(board -> board.getPosY() > screenY);
-        }
-        Log.i("i","remove bar");
-
-        // random generate the bars for half screen on top
-        bars.addAll(random_generate(screenY/2, screenX));
-        Log.i("reminder","finish update");
-        this.isPlaying = true;
-
-    }
-
-
-
 
 
 }
---
+++ app/src/main/java/com/example/android/gameapplication/GameContext.java
@@ -0,0 +1,163 @@
+package com.example.android.gameapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.android.gameapplication.GameClasses.Board;
+import com.example.android.gameapplication.GameClasses.CollisionUtils;
+import com.example.android.gameapplication.GameClasses.Jumper;
+import com.example.android.gameapplication.GameClasses.StaticBoard;
+import com.example.android.gameapplication.Sensors.OrientationMessage;
+import com.example.android.gameapplication.Sensors.OrientationSensor;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameContext extends View{
+
+    private OrientationSensor orientationSensor;
+    private boolean isPlaying, isUpdate = true;
+    public static int screenX, screenY;
+    private Paint paint;
+
+    public ArrayList<Board> boards;
+    public Jumper jumper;
+
+    private GameActivity activity;
+
+    public GameContext(Context contest, int screenX, int screenY) {
+        super(contest);
+
+        /** Init sensor variables*/
+        this.orientationSensor = new OrientationSensor(getContext());
+        EventBus.getDefault().register(this);
+
+        this.screenX = screenX;
+        this.screenY = screenY;
+
+        // random generate the boards for full screen
+        this.boards = random_generate(screenY, screenX);
+        int initBoardX = boards.get(0).getPosX();
+        int initBoardY = boards.get(0).getPosY();
+        Log.i("generate","initLoc"+initBoardX+"Y"+initBoardY);
+        this.jumper = new Jumper(getContext(),100,500,100,10f,
+                screenX, R.drawable.jumperone);
+
+        Log.i("generate","Game view");
+    }
+
+    /**
+     *
+     * @param canvas
+     */
+    @Override
+    public void onDraw(Canvas canvas) {
+        //super.onDraw(canvas);
+        Log.i("jumper loc","reach "+jumper.getPosY()+" screen" + screenY);
+        jumper.draw(canvas);
+        isPlaying = checkJumper();
+        for (Board board : boards){
+            board.draw(canvas);
+        }
+        invalidate();
+
+    }
+
+    private ArrayList<Board> random_generate(int startY, int width){
+        ArrayList<Board> newboards = new ArrayList<>();
+        Random random = new Random();
+        int y = startY;
+        int x = width/5;
+        while (y > 0){
+
+            Board bar = new StaticBoard(getContext(),
+                    x * random.nextInt(5),y,250,width,
+                    R.drawable.basic_board);
+            newboards.add(bar);
+//            Log.i(String.valueOf(bar.y),String.valueOf(bar.height));
+            y -= bar.getBoardHeight()*3*(random.nextInt(2)+1);
+        }
+        return newboards;
+    }
+
+    private Boolean checkJumper(){
+        if (jumper.getPosY()*2 < screenY){
+            Log.i("reminder","reach "+jumper.getPosY()+" screen" + screenY);
+            jumper.setPosY(jumper.getPosY()+screenY/2);
+            update();
+        }
+        if (jumper.getPosY() < screenY){
+            return false;
+        }
+        return true;
+    }
+
+    private void update () {
+        this.isPlaying = false;
+        // update the bars downward half screen
+
+        for (Board board : boards){
+            board.setPosY(board.getPosY()+screenY/2);
+        }
+        Log.i("i","update bar");
+
+        // remove the bar with y out of screen
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            boards.removeIf(board -> board.getPosY() > screenY);
+        }
+
+        Log.i("i","remove bar");
+
+        // random generate the bars for half screen on top
+        boards.addAll(random_generate(screenY/2, screenX));
+        Log.i("reminder","finish update");
+        this.isPlaying = true;
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.i("reminder","reach "+event.getY()+" screen" + screenY);
+        if (event.getY()*2 < screenY){
+            Log.i("i", "1/2");
+            update ();
+        }
+        return false;
+    }
+
+    /**
+     * @author Changwen Li
+     * @description Please get the value of changed sensor signal here. You may change the name of function.
+     * @param OrientationEvent see OrientationMessage.java
+     * */
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void orientationUpdate(OrientationMessage OrientationEvent) { // place to get sensor value from orientation
+        Log.d("[Subscription]" , "Orientations: " + String.valueOf(OrientationEvent.getOrientations()[2]));
+        Float moveX = 50*OrientationEvent.getOrientations()[2];
+        jumper.move(moveX,10f);
+
+        // not a good solution, as collision detection should be done within the context class
+        for (Board bar : boards){
+            if(CollisionUtils.JumperBoardCollision(jumper,bar))
+            {
+                jumper.setSpeedY(20f);
+            }
+            bar.move(10f,0f);
+        }
+
+    }
+
+
+
+
+}
