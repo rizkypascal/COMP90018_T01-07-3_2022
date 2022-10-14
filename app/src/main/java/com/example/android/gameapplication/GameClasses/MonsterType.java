a96e9ddca8ea00553ac291ec4c454126b707ecfc
Tony <tianbos@student.unimelb.edu.au>
2022-10-15 02:50:10 +1100

    create classes for monster and bullet

---
+++ app/src/main/java/com/example/android/gameapplication/GameClasses/MonsterType.java
@@ -0,0 +1,13 @@
+package com.example.android.gameapplication.GameClasses;
+
+/**
+ * @author Tony Shu
+ * @date 15/10/2022
+ * @desc
+ */
+public enum MonsterType {
+    QUIZ,
+    HOMEWORK,
+    EXAM,
+    TEST
+}
