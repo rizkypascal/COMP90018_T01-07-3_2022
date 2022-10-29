84e60ef89c01874edcfc3adf459b6cada9d46e30
Runzhe Hua <huarunzhe@gmail.com>
2022-10-29 14:56:04 +1100

    stacticboard become movable

--- app/src/main/java/com/example/android/gameapplication/games/StaticBoard.java
+++ app/src/main/java/com/example/android/gameapplication/games/StaticBoard.java
@@ -1,6 +1,9 @@
 package com.example.android.gameapplication.games;
 
 import android.content.Context;
+import android.util.Log;
+
+import java.util.Random;
 
 public class StaticBoard extends Board{
 
@@ -12,6 +15,26 @@ public class StaticBoard extends Board{
     @Override
     public void move(Float velocityX, Float velocityY)
     {
+        Random rand =new Random(25);
+        int i;
+        i=rand.nextInt(10);
+
+        if (i < 3){
+            Integer nextX = Math.round(velocityX * this.moveDirection) + posX;
+            Log.d("Board", "move: " + this.posX);
+            if (nextX >= screenSize - this.width/2){
+                this.moveDirection = -1;
+            }
+            else if (nextX <= this.width/2){
+                this.moveDirection = 1;
+                this.posX = this.width/2;
+            }
+            else{
+                this.posX = nextX;
+            }
+
+        }
         this.posY += Math.round(velocityY);
+
     }
 }
